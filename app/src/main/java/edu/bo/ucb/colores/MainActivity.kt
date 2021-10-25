5e07c7197b5a2bf909213e50550eb4de719c0edd
Javier Loayza Flores <javo_tralll@hotmail.com>
2021-10-25 11:01:57 -0400

    guias 2 y 3

--- app/src/main/java/edu/bo/ucb/colores/MainActivity.kt
+++ app/src/main/java/edu/bo/ucb/colores/MainActivity.kt
@@ -2,10 +2,26 @@ package edu.bo.ucb.colores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.Color
+import android.text.Layout
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnWhite = findViewById(R.id.btnWhite) as Button
+        val btnRed = findViewById(R.id.btnRed) as Button
+        val layoutPrincipal = findViewById(R.id.layoutPrincipal) as LinearLayout
+        btnWhite.setOnClickListener {
+            layoutPrincipal.setBackgroundColor(Color.WHITE)
+        }
+        btnRed.setOnClickListener({
+            layoutPrincipal.setBackgroundColor(Color.RED)
+        })
     }
 }
